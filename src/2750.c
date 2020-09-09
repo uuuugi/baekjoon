1e253ae47ec1615622d2fdee395a2c3c3b7c3fb7
uuuugi <67648064+uuuugi@users.noreply.github.com>
2020-09-09 22:11:48 +0900

    Create 2750.c

---
+++ src/2750.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int list[1001];
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &list[i]);
+
+	for(int i=0; i<n; i++)
+		for (int j = i + 1; j < n; j++)
+		{
+			if (list[i] > list[j])
+			{
+				int tmp;
+				tmp = list[i];
+				list[i] = list[j];
+				list[j] = tmp;
+			}
+		}
+	for (int i = 0; i < n; i++)
+		printf("%d\n", list[i]);
+
+	return 0;
+}
